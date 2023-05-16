496dfd2ba242fcb411b0b4ba85237669c3bffa3e
dantoper556 <556danbrown556@gmail.com>
2023-05-16 23:25:00 +0300

    changes

--- htm/main/forms.py
+++ htm/main/forms.py
@@ -4,6 +4,7 @@ from django.forms.utils import ErrorList
 from .models import *
 
 class ChooseCardForm(Form):
+    cnt = IntegerField(required=False, label="", widget=HiddenInput())
     cards = ChoiceField(label="", choices=((i, VideoCard.objects.all()[i].name) for i in range(len(VideoCard.objects.all()))))
 
         # self.cards.choices = tuple(tmp_l)
--- htm/main/views.py
+++ htm/main/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import *
 from .forms import *
+from django.forms import formset_factory
 
 def main_page(request):
     return render(request, 'main_page.html')
@@ -18,12 +19,26 @@ def calc_profit_page(request):
 
     data["was"] = 0
     data["form"] = ChooseCardForm()
+    data["cnt"] = 1
+    # data["forms"] = [ChooseCardForm()]
+    data["forms"] = formset_factory(ChooseCardForm, extra=data["cnt"])
+    # if (request.method == "POST"):
+    #     t1 = request.POST['form-0-cards']
+    #     print(t1)
     if (request.method == "POST"):
-        form = ChooseCardForm(request.POST)
-        if (form.is_valid()):
-            data["was"] = 1
-            data["chosen_card"] = VideoCard.objects.all()[int(form.cleaned_data["cards"])]
+        if ("inc" in request.POST):
+            print(request.POST)
+            data["cnt"] = int(request.POST['form-0-cnt']) + 1
+            data["forms"].extra = data["cnt"]
+            # t1 = ChooseCardForm(request.POST)
+            # if (t1.is_valid()):
+            #     if (t1.cleaned_data["cnt"] != None):
+            #         data["cnt"] = t1.cleaned_data["cnt"] + 1
+            #         data["forms"] = [ChooseCardForm() for i in range(data["cnt"])]
+            #         return render(request, 'calc_profit_page.html', context=data)
+            #     # return HttpResponse(t1.cleaned_data["cnt"])
+            # return HttpResponse("not ok")
         else:
-            return HttpResponse("not hooray")
+            return HttpResponse("ok2")
         # return render(request, 'calc_profit_page.html', context={"cards": ['testik']})
     return render(request, 'calc_profit_page.html', context=data)
