b69fc9814ea262a54269f7fcf91de05357c8a6af
dantoper556 <556danbrown556@gmail.com>
2023-05-18 20:47:59 +0300

    changes

--- htm/main/forms.py
+++ htm/main/forms.py
@@ -3,7 +3,11 @@ from django.forms import *
 from django.forms.utils import ErrorList
 from .models import *
 
+class ElectricityPriceForm(Form):
+    val = IntegerField(label="electricity price in usd")
+
 class ChooseCardForm(Form):
+    electricity = IntegerField(label="electricity price in usd")
     cnt = IntegerField(required=False, label="", widget=HiddenInput())
     cards = ChoiceField(label="", choices=((i, VideoCard.objects.all()[i].name) for i in range(len(VideoCard.objects.all()))))
     quantity = IntegerField(required=True, label="", min_value=1)
