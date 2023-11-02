0d875e0c7de7ccfc8bd54bd688713a3a9cd5d3da
dantoper556 <120361032+dantoper556@users.noreply.github.com>
2023-11-02 17:47:52 +0300

    changes

--- htm/main/tests.py
+++ htm/main/tests.py
@@ -6,21 +6,20 @@ from .presenters import make_table_vc
 class test1(TestCase):
     def setUp(self) -> None:
         self.config = dict()
-        print(VideoCard.objects.all())
+        VideoCard.objects.create(name="RTX 3070", hashrate_no_code="3070")
         for el in VideoCard.objects.all():
             
-            if (el.hashrate_no_code == "2060s"):
+            if (el.hashrate_no_code == "3070"):
                 self.config[el] = 10
         self.data = dict()
         self.data["elec"] = 0.1
-        print(self.config)
-        
-        
-    
+
     def test(self):
+        VideoCard.objects.create(name="RTX 3070", hashrate_no_code="3070")
+        CryptoCoin.objects.create(name="Conflux", hashrate_no_code="CFX")
         ret = calc_config_profit(self.config, self.data["elec"])
         tbl = make_table_vc(self.data, ret)
-        ans1 = "0 $"
-        self.assertEqual(self.config, ans1)
+        self.assertTrue(abs(float(tbl[0]["hsh"].split()[0]) / (579.2) - 1) < 0.01)
+        self.assertTrue(abs(float(tbl[0]["pwr_cons"].split()[0]) / (140 * 10 * 24 / 1000) - 1) < 0.05)
 
 
